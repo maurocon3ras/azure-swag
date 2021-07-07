6c06af66d716456e789286a604df4c5a0c813761
Mauricio Contreras <m_contreras@outlook.cl>
2021-07-07 17:33:10 -0400

    updates

--- app.js
+++ app.js
@@ -4,25 +4,22 @@ async function displayQuote() {
 
   try {
     // Fetch quote object from API
-    let response = await fetch('/api/quote');
-    quote = await response.json();
+    let randomNumber = Math.floor(Math.random() * (10 - 1) + 1);
+    let quoteResponse = await fetch('/api/quote?id='+randomNumber+"&partitionKeyValue="+randomNumber);
+    quote = await quoteResponse.json();
 
-    // Fetch image from API
-    let imgrsp = await fetch('/api/image');
-    image = await imgrsp.json();
-
+    // Fetch image object from API
+    image = await fetch('/api/image');
     // Preload image
-    response = await fetch(image.image);
-    const imag = await response.blob()
-    image.image = URL.createObjectURL(imag);
+    image = URL.createObjectURL(image);
   } catch (error) {
     quote = { text: `Could not get quote: ${error.message}` };
   }
 
   // Update background image
-  document.body.style.background = image.image ? `url(${image.image}) center / cover` : '#f43';
+  document.body.style.background = image ? `url(${image}) center / cover` : '#f43';
 
   // Update text
   const div = document.getElementById('quote');
-  div.textContent = quote.text;
+  div.textContent = quote.message;
 }
